4de719efc383bebedc0c5702e7a8ba1252d4220f
Ralphjan Tocong <tocongralphjan@gmail.com>
2026-07-10 21:55:15 +0800

    Test

--- src/utils/dayNumber.js
+++ src/utils/dayNumber.js
@@ -1,6 +1,7 @@
 export const TARGET_DATE = new Date(2027, 7, 16) // August 16, 2027
 const MS_PER_DAY = 1000 * 60 * 60 * 24
 
+//
 export function getDayNumber(date) {
   const start = new Date(date.getFullYear(), date.getMonth(), date.getDate())
   const startOfTarget = new Date(
